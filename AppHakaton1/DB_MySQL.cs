929709996e750f49abd4ddf78ba3fc4622b80226
Vsevolod <ovchinnikov.seva@yandex.ru>
2020-05-09 21:31:51 +0300

    load project

---
+++ AppHakaton1/DB_MySQL.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
+    public class DB_MySQL
+    {
+        String connString;
+        string host;
+        int port;
+        string database;
+        string username;
+        string password;
+
+        //Объект для подключения к БД
+
+        MySqlConnection obj;
+        //Объект для выполнения SQL-запроса
+        MySqlCommand objComand;
+
+        /*
+         * openSessionMySQL - открываем соединение с бд
+         * statusOpenSession - проверка подключения к бд
+         * CreatingNewRowTimetable - отдельный метод для внесения данных в таблицу с расписанием
+         * DeletRow_id - метод удаляющий данные в любой таблице по id
+         * DeletRowTimeT_FlightNumber - удаляет запись из таблицы с расписанием по номеру рейса
+         * getTimeTimetable - получить расписание. НЕ ДОДЕЛАЛ!!!
+         */
+
+        public DB_MySQL(string Host, int Port, string Database, string Username, string Password)
+        {
+            host = Host;
+            port = Port;
+            database = Database;
+            username = Username;
+            password = Password;
+        }
+
+        public void openSessionMySQL()
+        {
+            connString = "Server=" + host + ";Database=" + database + ";port=" + port + ";User Id=" + username + ";password=" + password;
+            obj = new MySqlConnection(connString);
+            try
+            {
+                Console.WriteLine("Try open session");
+                obj.Open();
+            }
+            catch
+            {
+                Console.WriteLine("Error. Session not open!");
+            }
+        }
+
+        public bool statusOpenSession()
+        {
+           return obj.Ping();
+        }
+
+        public void CreatingNewRowTimetable(DateTime FlightDate, TimeSpan ScheduledTime, int AirlineCode, int FlightNumber, string FlagArrivalDeparture, string TypeAircraft, string AParking, string ParkingSector, string NameAirline)
+        {
+            try
+            {
+                string sql = "INSERT INTO `Timetable`  (`FlightDate`, `ScheduledTime`, `AirlineCode`, `FlightNumber`, `FlagArrivalDeparture`, `TypeAircraft`, `AParking`,`ParkingSector`,`NameAirline`) VALUES ('" + FlightDate + "', '" + ScheduledTime + "', '" + AirlineCode + "', '" + FlightNumber + "', '" + FlagArrivalDeparture + "', '" + TypeAircraft + "', '" + AParking + "', '" + ParkingSector + "', '" + NameAirline + "')";
+                objComand = new MySqlCommand(sql, obj);
+                objComand.ExecuteScalar();
+
+                Console.WriteLine("Create new note");
+            }
+            catch
+            {
+                Console.WriteLine("Error. The add request was not executed");
+            }
+        }
+
+        public void DeletRow_id(int id, string NameTable)
+        {
+            try
+            {
+                string sql = "DELETE FROM `" + NameTable + "` WHERE id =" + id;
+                objComand = new MySqlCommand(sql, obj);
+                objComand.ExecuteScalar();
+
+                Console.WriteLine("Note is delete");
+            }
+            catch
+            {
+                Console.WriteLine("Error. The add request was not executed");
+            }
+        }
+
+        public void DeletRowTimeT_FlightNumber(int FlightNumber)
+        {
+            try
+            {
+                string sql = "DELETE FROM `Timetable` WHERE FlightNumber =" + FlightNumber;
+                objComand = new MySqlCommand(sql, obj);
+                objComand.ExecuteScalar();
+
+                Console.WriteLine("Note is delete");
+            }
+            catch
+            {
+                Console.WriteLine("Error. The add request was not executed");
+            }
+
+        }
+
+        public void getTimeTimetable(int FlightNumber)
+        {
+            var result = new List<string>();
+            try
+            {
+                string sql = "SELECT `AirlineCode` FROM `Timetable` WHERE FlightNumber =" + FlightNumber;
+                objComand = new MySqlCommand(sql, obj);
+
+                //Console.WriteLine(objComand.ExecuteScalar().ToString());
+                var reader = objComand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    result.Add(reader.GetString(0));
+                }
+                reader.Close();
+
+                string[] a = result.ToArray();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error. The add request was not executed = " + ex.Message);
+            }
+        }
+    }
---
+++ AppHakaton1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Office.Interop;
+using System.IO;
+using Microsoft.Win32;
+
+namespace AppHakaton1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            /*Создаем объект подключения к бд objSQL. В конструктор передаем параметры подключения */
+            DB_MySQL objSQL = new DB_MySQL("sql7.freesqldatabase.com", 3306, "sql7338923", "sql7338923", "bc9vSYmu5u");
+
+            /*Открываем сессию передачи данных в бд */
+            objSQL.openSessionMySQL();
+
+            /*Проверяем доступность к бд */
+            MessageBox.Show("Доступ есть? " + objSQL.statusOpenSession().ToString());
+
+            
+        }
+    }
+}
